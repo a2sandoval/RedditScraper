90b999df452ece7a6af2a24bf19386f172e89b73
01100001 <a01100001@MacBook-Pro-7.hitronhub.home>
2018-10-13 23:00:42 -0700

    added heroku assigned port

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
 
 // Initialize Express
 var app = express();
@@ -119,6 +118,6 @@ app.post("/articles/:id", function(req, res) {
 });
 
 // Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
